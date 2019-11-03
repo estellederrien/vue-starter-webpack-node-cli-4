5f331686a89e8f79d299afbbbb764de0b4fc359f
nicolas <nicolas.huleux@gmail.com>
2019-11-03 19:45:36 +0100

    Added Users component Spec unit test

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,57 +1,68 @@
 <template>
-    
-   <div class="container">
-          <div class="row" > 
-                <div  v-for="user in users"  class="col-sm col-xs-12">
-                    <div  class="card " style ="margin-bottom : 20px"  >
-                        <img src="../assets/img/defaut.png"  class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">{{user.nom}}</h5>
-                            <p class="card-text">Voici la description de mon profil .</p>
-                            <a v-on:click="route(user._id)" class="btn btn-primary float-right">Profil</a>
-                           <!-- <span class="badge badge-warning"> {{ info }}</span>  -->
-                        </div>
-                    </div>
+<div class="container">
+    <div class="row">
+        <div v-for="user in users" class="col-sm col-xs-12">
+            <div class="card " style="margin-bottom : 20px">
+                <img src="../assets/img/defaut.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">{{user.nom}}</h5>
+                    <p class="card-text">Voici la description de mon profil .</p>
+                    <a v-on:click="route(user._id)" class="btn btn-primary float-right">Profil</a>
+                    <!-- <span class="badge badge-warning"> {{ info }}</span>  -->
                 </div>
             </div>
         </div>
+    </div>
+</div>
 </template>
 
 <script>
 import axios from "axios";
- 
- export default {
+
+export default {
     name: 'Users',
-    
+
     methods: {
-        route: function(_id) {
+        route: function (_id) {
             alert(_id);
             this.$router.push("/user/" + _id);
         },
-        getUsers: function() {
-        axios
-            .post(this.server + "getUsers")
-            .then(response => {
-            this.users = response.data;
-            })
-            .catch(function(error) {
-            console.log(error);
-            });
+        getUsers: function () {
+            axios
+                .post(this.server + "getUsers")
+                .then(response => {
+                    this.users = response.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
         }
     },
-    created: function() {
+    created: function () {
         this.getUsers();
     },
-    data () {
-      return {
-        msg: 'Welcome to Crypto Info',users: [
-        { nom: "Derrien", id: "1", img: "img/h1.jpg" },
-        { nom: "Floril", id: "2", img: "img/f1.jpeg" },
-        { nom: "Daril", id: "3", img: "img/h2.jpeg" }
-      ],
-      }
+    data() {
+        return {
+            msg: 'Welcome to Crypto Info',
+            users: [{
+                    nom: "Derrien",
+                    id: "1",
+                    img: "img/h1.jpg"
+                },
+                {
+                    nom: "Floril",
+                    id: "2",
+                    img: "img/f1.jpeg"
+                },
+                {
+                    nom: "Daril",
+                    id: "3",
+                    img: "img/h2.jpeg"
+                }
+            ],
+        }
     }
-  }
+}
 </script>
 
 <style>
