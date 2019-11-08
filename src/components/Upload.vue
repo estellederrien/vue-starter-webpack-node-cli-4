40163829522dbe7299cc0a60c273d6657b0313bb
nicolas <nicolas.huleux@gmail.com>
2019-11-08 18:17:45 +0100

    Separating node pictures and files directories

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -1,22 +1,17 @@
 <template>
-
     <div class="col-lg-12">
         <a class="btn btn-primary btn-block" v-on:click="submitFile()">Télécharger</a>
-        
+
         <label>
             <input type="file" id="file" ref="file" v-on:change="handleFileUpload()" />
-          
-        </label><br>
-          
-    </div>
 
+        </label><br>
+
+    </div>
 </template>
 
 <script>
-
-
 import axios from "axios";
-
 
 export default {
     /*
@@ -34,54 +29,29 @@ export default {
           Submits the file to the server
         */
         submitFile() {
-            /*
-                    Initialize the form data
-                */
+            /*Initialize the form data*/
             let formData = new FormData();
 
-            /*
-                Add the form data we need to submit
-            */
+            /*Add the form data we need to submit*/
             formData.append('file', this.file);
 
-            /*
-              Make the request to the POST /single-file URL
-            */
-
-           /*  $.post( this.server + 'images',formData, function( data ) {
-                alert('ok');
-                });
- */
-
-
-
-
-
-            axios.post(this.server + 'images',
-                    formData, {
-                      crossdomain: true,
-                        headers: {
-                            'Content-Type': 'multipart/form-data'
-                        }
+            /*Make the request to the POST /single-file URL*/
+             axios.post(this.server + 'images',
+                formData, {
+                    crossdomain: true,
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
                     }
-                ).then(response => {
-                    // Send to parent component
-                     this.$emit('filename', response.data.filename)
-                    if(response == undefined){
-
-                      
-                    }
-                    console.log(response);
-                      console.log(JSON.stringify(response))
-                    //the following errors out because response is undefined
-                    if (response.data && response.data.success === true) {
-                    }
-                }).catch(err => {
-                    //catch never triggered
-                        console.log(err)
-                        console.log(err.response) 
-                        console.log(JSON.stringify(err))
-                });
+                }
+            ).then(response => {
+                // Send to parent component
+                this.$emit('filename', response.data.filename)
+            }).catch(err => {
+                //catch never triggered
+                console.log(err)
+                console.log(err.response)
+                console.log(JSON.stringify(err))
+            });
         },
 
         /*
--- src/components/Uploadfiles.vue
+++ src/components/Uploadfiles.vue
@@ -7,9 +7,9 @@
         <a v-on:click="submitFiles" class="btn btn-primary">Submit</a>
 
         <div class="large-12 medium-12 small-12 cell">
-            <div v-for="(file, key) in files" class="file-listing">{{ file.name }} <span class="remove-file" v-on:click="removeFile( key )">Remove</span></div>
-            </div>
-            <br>
+            <div v-for="(file, key) in files" class="file-listing">{{ file.name }} <span class="remove-file btn btn-primary" v-on:click="removeFile( key )">Remove</span></div>
+        </div>
+        <br>
     </div>
 </div>
 </template>
@@ -29,69 +29,52 @@ export default {
     },
 
     methods: {
-        /*
-          Submits the file to the server
-        */removeFile( key ){
-            this.files.splice( key, 1 );
-            },
-        submitFiles : function() {
-         
-            /*
-                    Initialize the form data
-                */
+        /*Submits the file to the server*/
+        removeFile(key) {
+            this.files.splice(key, 1);
+        },
+        submitFiles: function () {
+
+            /*Initialize the form data*/
             var formData = new FormData();
-          
+
             /*
-            Iteate over any file sent over appending the files
+            Iterate over any file sent over appending the files
             to the form data.
             */
-                console.log(this.files);
-
-           /*  for (var i = 0; i < this.files.length; i++) {
-              
-               var file = this.files[i];
-
-                formData.append('files[' + i + ']', file);
-            } */
-
-            /* formData.append(file,this.files[0]);
-            formData.append(file,this.files[1]); */
-
             console.log(this.files);
-            /*
-                Make the request to the POST /multiple-files URL
-                */
-               var formData = new FormData(); 
-               for (var i = 0; i < this.files.length; i++) {
-              
-               var file = this.files[i];
+
+            for (var i = 0; i < this.files.length; i++) {
+
+                var file = this.files[i];
 
                 formData.append('file', file);
 
-                
-
-               }
-               axios.post(this.server + 'files',
+            }
+            /*Make the request to the POST /multiple-files URL*/
+            axios.post(this.server + 'files',
                     formData, {
-                        headers: { crossdomain: true,
+                        headers: {
+                            crossdomain: true,
                             'Content-Type': 'undefined'
                         }
                     }
                 ).then(function (response) {
+                    // Send to filenames to the parent component
+                    this.$emit('filenames', response.data.filenames)
                 })
-                .catch(function (error) {
+                .catch(function (error) 
+                {
+                    alert('Error : please contact an admin')
+                    console.log(error);
                 });
 
-        }, 
-        handleFileUploads : function() {
-                /*
-            Handles a change on the file upload
-            */
-
+        },
+        handleFileUploads: function () {
+            /*Handles a change on the file upload*/
             this.files = this.$refs.files.files;
-            console.log(this.files);
-        } 
-       
+        }
+
     }
 }
 </script>
