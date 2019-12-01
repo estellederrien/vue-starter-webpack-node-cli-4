a783e0b7b2803c8803cdf74e4bbe8e3f96f08f65
nicolas <nicolas.huleux@gmail.com>
2019-12-01 15:17:19 +0100

    Adding all of the unit tests - part 1

--- tests/unit/filters.spec.js
+++ tests/unit/filters.spec.js
@@ -24,6 +24,7 @@ Vue.use(Notifications, { velocity });
 // THE WRAPPER CONTAIN MY MOUNTED COMPONENT, JUST LIKE IN THE REAL LIFE
 const wrapper = mount(Filters)
 
+// LETS START AND DO SOME TESTS ! - SIMPLY TYPE NPM RUN TEST INSIDE THE CONSOLE AT THE ROOT OF THE APP
 describe("Filters test", () => {
   
   // FUNCTIONS PRESENCE
