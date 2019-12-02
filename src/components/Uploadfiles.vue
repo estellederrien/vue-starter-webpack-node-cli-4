5a6068d2519cf8b4cefa44f3468dcb87d6aa780a
nicolas <nicolas.huleux@gmail.com>
2019-12-02 18:29:10 +0100

    Adding the correct then catch syntax + removing alerts, replacing by $notify

--- src/components/Uploadfiles.vue
+++ src/components/Uploadfiles.vue
@@ -1,120 +1,120 @@
 <template>
-  <div class="container">
+<div class="container">
     <div class="col-md-12">
-      <label
-        >Fichiers multiples<br />
-        <input
-          name="file"
-          type="file"
-          id="files"
-          ref="files"
-          multiple
-          @change="handleFileUploads"
-        />
-      </label>
-      <a v-on:click="submitFiles" class="btn btn-primary btn-block"
-        >Soumettre</a
-      >
+        <label>Fichiers multiples<br />
+            <input name="file" type="file" id="files" ref="files" multiple @change="handleFileUploads" />
+        </label>
+        <a v-on:click="submitFiles" class="btn btn-primary btn-block">Soumettre</a>
 
-      <!-- LOADING PIC -->
-      <img v-if="loading" style="width:100px" src="../assets/img/JointRevolvingAntelopegroundsquirrel-size_restricted.gif"></img>
+        <!-- LOADING PIC -->
+        <img v-if="loading" style="width:100px" src="../assets/img/JointRevolvingAntelopegroundsquirrel-size_restricted.gif"></img>
 
-      <!-- REALTIME FILE MANAGEMENT -->
-      <table class="table  table-dark">
-        <tbody>
-          <tr v-for="(file, key) in files" class="file-listing">
-            <th>{{ file.name }}</th>
-            <th>
-              <span
-                class="remove-file btn btn-primary"
-                v-on:click="removeFile(key)"
-                >Remove</span
-              >
-            </th>
-          </tr>
-        </tbody>
-      </table>
+        <!-- REALTIME FILE MANAGEMENT -->
+        <table class="table  table-dark">
+            <tbody>
+                <tr v-for="(file, key) in files" class="file-listing">
+                    <th>{{ file.name }}</th>
+                    <th>
+                        <span class="remove-file btn btn-primary" v-on:click="removeFile(key)">Remove</span>
+                    </th>
+                </tr>
+            </tbody>
+        </table>
     </div>
-  </div>
+</div>
 </template>
 
 <script>
 import axios from "axios";
 
 export default {
-  /*
-      Defines the data used by the component
-    */
-  name: "uploadfiles",
-  props: ["filenames"],
-  data() {
-    return {
-      files: "",
-      loading: false
-    };
-  },
+    /*
+        Defines the data used by the component
+      */
+    name: "uploadfiles",
+    props: ["filenames"],
+    data() {
+        return {
+            files: "",
+            loading: false
+        };
+    },
 
-  methods: {
-    
-    removeFile(key) {
-      var newList = [];
+    methods: {
 
-      for (var i = 0; i < this.files.length; i++) {
-        if (i !== key) {
-          newList.push(this.files[i]);
+        removeFile(key) {
+            var newList = [];
+
+            for (var i = 0; i < this.files.length; i++) {
+                if (i !== key) {
+                    newList.push(this.files[i]);
+                }
+            }
+            this.files = newList;
+        },
+        submitFiles: function () {
+            this.loading = true;
+            /*Initialize the form data*/
+            var formData = new FormData();
+
+            /*
+                  Iterate over any file sent over appending the files
+                  to the form data.
+                  */
+            console.log(this.files);
+
+            for (var i = 0; i < this.files.length; i++) {
+                var file = this.files[i];
+
+                formData.append("file", file);
+            }
+
+            /*Make the request to the POST /multiple-files URL*/
+            axios
+                .post(this.server + "files", formData, {
+                    headers: {
+                        crossdomain: true,
+                        "Content-Type": "undefined"
+                    }
+                })
+
+                .then(response => {
+                    this.loading = false;
+
+                    this.$notify({
+                        type: 'success',
+                        group: 'foo',
+                        title: 'Hey! ',
+                        text: 'Files have been uploaded'
+                    });
+                    // Send to filenames to the parent component, Displaying them in the user component
+                    this.$emit("myfilenamesevent", response.data);
+                    // INITIALIZING FILES TO GET READY TO UPLOADS SOME NEWS !
+                    this.files = [];
+
+                })
+                .catch(error => {
+                    this.$notify({
+                        type: 'error',
+                        group: 'foo',
+                        title: 'Hey! ',
+                        text: 'Permission is missing ! -> <br> ' + error
+                    });
+                    console.log(error);
+
+                });
+
+        },
+        handleFileUploads: function () {
+            /*Handles a change on the file upload*/
+            this.files = this.$refs.files.files;
+            this.$notify({
+                type: 'success',
+                group: 'foo',
+                title: 'Bien choisis ! ',
+                text: 'Cliquez sur Soumettre pour placer les fichiers sur le serveur !'
+            });
         }
-      }
-      this.files = newList;
-    },
-    submitFiles: function() {
-      this.loading = true;
-      /*Initialize the form data*/
-      var formData = new FormData();
-      var self = this;
-      /*
-            Iterate over any file sent over appending the files
-            to the form data.
-            */
-      console.log(this.files);
-
-      for (var i = 0; i < this.files.length; i++) {
-        var file = this.files[i];
-
-        formData.append("file", file);
-      }
-
-      /*Make the request to the POST /multiple-files URL*/
-      axios
-        .post(this.server + "files", formData, {
-          headers: {
-            crossdomain: true,
-            "Content-Type": "undefined"
-          }
-        })
-        .then(function(response) {
-           self.loading = false;
-          alert("Files have been uploaded ");
-         
-          // Send to filenames to the parent component, Displaying them in the user component
-          self.$emit("myfilenamesevent", response.data);
-          // INITIALIZING FILES TO GET READY TO UPLOADS SOME NEWS !
-          self.files = [];
-        })
-        .catch(function(error) {
-          alert("Error : please contact an admin");
-          console.log(error);
-        });
-    },
-    handleFileUploads: function() {
-      /*Handles a change on the file upload*/
-      this.files = this.$refs.files.files;
-      this.$notify({
-        type: 'success',
-        group: 'foo',
-        title: 'Bien choisis ! ',
-        text: 'Cliquez sur Soumettre pour placer les fichiers sur le serveur !'
-        });
     }
-  }
 };
 </script>
