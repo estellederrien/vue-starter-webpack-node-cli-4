94abce8bad3b6bc5b16219329aa04806a24cd567
nicolas <nicolas.huleux@gmail.com>
2019-12-24 12:37:50 +0100

    Global restyling + Repairing smartgit bug

--- src/components/Uploadfiles.vue
+++ src/components/Uploadfiles.vue
@@ -1,39 +1,38 @@
-<template >
-
-
-        <div class ="row border border-dark rounded type" >
-            
-            <div class="col-md-12">
-                <p class="bg-warning">Uploadeur de Fichiers multiples</p>
-            </div>
-            
-            <div class="col-md-6 ">
-                <div class="FileListDiv" >
-                 <input class ="float-left" name="file" type="file" id="files" ref="files" multiple @change="handleFileUploads" />
-                 <!-- LOADING PIC -->
+<template>
+<div class="card">
+    <div class="card-body">
+        <h5 class="card-title">
+            <i class="fas fa-plus"></i> Télécharger des fichiers sur le serveur
+        </h5>
+        <h6 class="card-subtitle mb-2 text-muted">Maintenez la touche TAB enfoncée...</h6>
+        <div class="row">
+       
+           
+                <input  name="file" type="file" id="files" ref="files" multiple @change="handleFileUploads" />
+                <!-- LOADING PIC -->
                 <img v-if="loading" style="width:100px" src="../assets/img/JointRevolvingAntelopegroundsquirrel-size_restricted.gif"></img>
-              
-               
-                </div>
-            </div> 
-            <div class="col-md-6">
-                <label v-if="files">Preview :</label>
-                <!-- REALTIME FILE MANAGEMENT -->
-                <table class="table table-dark table-sm">
-                    <tbody>
-                        <tr v-for="(file, key) in files" >
-                            <th>{{ file.name }}</th>
-                            <th>
-                                <span class="remove-file btn btn-primary" v-on:click="removeFile(key)">-</span>
-                            </th>
-                        </tr>
-                    </tbody>
-                </table>
-                  <a v-if="files" v-on:click="submitFiles" class="btn btn-primary btn-block btn-small" style="color:white; font-size:0.8em;">Ok, placer les fichiers  sur le serveur </a>
-            </div>
-            </div>
-       </div>
- 
+
+            
+        
+        <label v-if="files">Preview :</label>
+        <!-- REALTIME FILE MANAGEMENT -->
+        <table class="table table-dark table-sm">
+            <tbody>
+                <tr v-for="(file, key) in files">
+                    <th>{{ file.name }}</th>
+                    <th>
+                        <span class="remove-file btn btn-primary" v-on:click="removeFile(key)">-</span>
+                    </th>
+                </tr>
+            </tbody>
+        </table>
+        <a v-if="files" v-on:click="submitFiles" class="btn btn-primary btn-block btn-small" style="color:white; font-size:0.8em;">Ok, placer les fichiers sur le serveur </a>
+      
+        </div>
+    </div>
+
+</div>
+
 
 </template>
 
@@ -131,29 +130,30 @@ export default {
     }
 };
 </script>
-<style scoped>
 
-.type{
-    font-size:0.8em;
+<style scoped>
+.type {
+    font-size: 0.8em;
     margin-bottom: 20px;
 }
 
 @media (max-width: 768px) {
-  .FileListDiv {
-    font-size: 11px;
-    padding: 4px 6px;
-    height:15px;
-  }
-  .tb{
-    font-size: 11px;
-  }
+    .FileListDiv {
+        font-size: 11px;
+        padding: 4px 6px;
+        height: 15px;
+    }
+
+    .tb {
+        font-size: 11px;
+    }
 }
 
 @media (min-width: 768px) {
-  .FileListDiv {
-    font-size: 12px;
-    padding: 6px 12px;
-     height:200px;
-  }
+    .FileListDiv {
+        font-size: 12px;
+        padding: 6px 12px;
+        height: 200px;
+    }
 }
 </style>
