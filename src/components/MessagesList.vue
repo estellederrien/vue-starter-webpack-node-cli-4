40a84cacd2f2bc3cccb9b9448ab0418342fc49e0
Nicolas HULEUX <nicolas.huleux@gmail.com>
2020-01-15 14:48:13 +0100

    Adding a message list component to the profile component

---
+++ src/components/MessagesList.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <!--  {{this._id}}
+    {{$user._id}}-->
+    <!-- <h6 class="dropdown-header">Message Center</h6> -->
+
+    <span v-for="m in messages">
+      <button class="btn btn-block btn-primary">
+        <div class="dropdown-list-image mr-3">
+          <img
+            class="rounded-circle"
+            src="https://source.unsplash.com/fn_BT9fwg_E/60x60"
+            :src="server + 'img/' + m.img"
+            @error="replaceByDefault"
+            alt
+          />
+          <div class="small text-gray-500">{{ m.senderName }}</div>
+          <div class="status-indicator bg-success"></div>
+        </div>
+        {{ m.date }}
+        <div class="font-weight-bold">
+          <div class="text-truncate">{{ m.content }}</div>
+        </div>
+      </button>
+    </span>
+    <a></a>
+
+    <!-- <a class="dropdown-item text-center small text-gray-500" href="#">Read More Messages</a> -->
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+axios.defaults.withCredentials = true;
+export default {
+  name: "messageslist",
+  props: ["_id"],
+  data() {
+    return {
+      messages: []
+    };
+  },
+
+  methods: {
+    replaceByDefault(e) {
+      e.target.src = this.server + "defaut.png";
+    },
+    getMessages: function() {
+      axios
+        .post(this.server + "getUserMessages", {
+          _id: this._id
+        })
+        .then(response => {
+          this.messages = response.data;
+        })
+        .catch(error => {
+          console.log(error);
+          this.$notify({
+            type: "error",
+            group: "foo",
+            title: "Hey! ",
+            text: error
+          });
+        });
+    }
+  },
+  created: function() {
+    this.getMessages();
+  }
+};
+</script>
