372285b88b49646dd4a2b1bf6f3560411d6c8c75
Nicolas HULEUX <nicolas.huleux@gmail.com>
2020-08-03 23:28:33 +0200

    Refactoring the whole file and pictures uploading system : PART 2 : Settings the cruds correctly .

--- cruds/filesCrud.js
+++ cruds/filesCrud.js
@@ -13,43 +13,38 @@
   NICOLAS ESTELLE HULEUX
 */
 
-module.exports = function(app, db, permissions) {
+module.exports = function (app, db, permissions) {
+  /* ************************************* NODE MODULES ****************************************************** */
 
-/* ************************************* NODE MODULES ****************************************************** */
+  var cloudinary = require("cloudinary") // Managing the cloudinary server pictures storing
+  var Client = require("ssh2-sftp-client") // Managing the SFTP server connexion
+  var multer = require("multer") // Managing multiples files uploads
+  const sharp = require("sharp") // Resizing pictures module
+  var fs = require("fs") // Managing the file system
+  const bodyParser = require("body-parser")
 
-var cloudinary    = require('cloudinary'); // Managing the cloudinary server pictures storing
-var Client        = require('ssh2-sftp-client'); // Managing the SFTP server connexion
-var multer        = require("multer"); // Managing multiples files uploads
-const sharp       = require("sharp"); // Resizing pictures module 
-var fs            = require("fs"); // Managing the file system
-const bodyParser  = require("body-parser");
+  /* ************************************* STORAGE SERVERS CONNEXIONS PARAMS ****************************************************** */
 
-
-/* ************************************* STORAGE SERVERS CONNEXIONS PARAMS ****************************************************** */
-
-
-/* 
+	/* 
    CLOUDINARY PARAMS
   'STORING PICTURES ON THE CLOUD' 
    YOU HAVE TO OWN A CLOUDINARY ACCOUNT 
 */
 
-cloudinary.config({ 
-  cloud_name: 'ddq5asuy2', 
-  api_key: '354237299578646', 
-  api_secret: '3UWkrND91MW3jhmGecvp77uetvQ' 
-});
+  cloudinary.config({
+    cloud_name: "ddq5asuy2",
+    api_key: "354237299578646",
+    api_secret: "3UWkrND91MW3jhmGecvp77uetvQ",
+  })
 
-
-/* 
+	/* 
   FTP SERVER PARAMS
   'STORING FILES AND PICTURES ON MY PERSONNAL FTP' 
   YOU HAVE TO OWN A FTP SERVER
 */
 
-
-// 1. NODE MODULE : FTP
-/*  var Client = require('ftp');
+  // 1. NODE MODULE : FTP
+	/*  var Client = require('ftp');
  let config = {
   host: "ftpupload.net",
   user: "epiz_25107181",
@@ -68,26 +63,26 @@ cloudinary.config({
   c.connect(config); 
 */
 
+  let sftp = new Client()
 
-
-let sftp = new Client();
-
-sftp.connect({
-   host: "test.rebex.net",
+  sftp.connect({
+    host: "test.rebex.net",
     user: "demo",
     password: "password",
-    port:22
-}).then(() => {
-  return sftp.list('/');
-}).then(data => {
-  console.log(data, 'the data info');
-}).catch(err => {
-  console.log(err, 'catch error');
-});
+    port: 22,
+  })
+    .then(() => {
+      return sftp.list("/")
+    })
+    .then((data) => {
+      console.log(data, "the data info")
+    })
+    .catch((err) => {
+      console.log(err, "catch error")
+    })
 
-
-// 3. NODE MODULE : multer-ftp NOT WORKING
- /*    var FTPStorage = require('multer-ftp')
+  // 3. NODE MODULE : multer-ftp NOT WORKING
+	/*    var FTPStorage = require('multer-ftp')
 
     var  storage = new FTPStorage({
         basepath: '/htdocs/files',
@@ -98,12 +93,11 @@ sftp.connect({
             secure: false
         }
     }) */
-// end multer-ftp
+  // end multer-ftp
 
+  // 4. NODE MODULE : multer-sftp NOT WORKING
 
-// 4. NODE MODULE : multer-sftp NOT WORKING
-
-/* var sftpStorage = require('multer-sftp')
+	/* var sftpStorage = require('multer-sftp')
  
 var storage = sftpStorage({
   sftp: {
@@ -120,116 +114,150 @@ var storage = sftpStorage({
   }
 }) */
 
+  /* ************************************* MULTER FILE HANDLINGS ****************************************************** */
 
-
- /* ************************************* FILE HANDLINGS ****************************************************** */  
-
-    
   // MULTER PARAMS FOR IMAGES STORAGE
   var storageImages = multer.diskStorage({
-        destination: function(req, file, cb) {
-            cb(null, "./uploads/img");
-        },
-        filename: function(req, file, cb) {
-            let ext = file.originalname.substring(file.originalname.lastIndexOf("."), file.originalname.length);
-            let myUniqueFileName = file.originalname + "-" + Date.now() + ext ;
-            cb(null,myUniqueFileName);
-            
-          
-        }
-    });
+    destination: function (req, file, cb) {
+      cb(null, "./uploads/img")
+    },
+    filename: function (req, file, cb) {
+      let ext = file.originalname.substring(
+        file.originalname.lastIndexOf("."),
+        file.originalname.length,
+      )
+      let myUniqueFileName = file.originalname + "-" + Date.now() + ext
+      cb(null, myUniqueFileName)
+    },
+  })
 
-    // MULTER PARAMS FOR FILES STORAGE
-    var storageFiles = multer.diskStorage({
-        destination: function(req, file, cb) {
-            cb(null, "./uploads/files");
-        },
-        filename: function(req, file, cb) {
-            let ext = file.originalname.substring(file.originalname.lastIndexOf("."), file.originalname.length);
-            cb(null, file.originalname + "-" + Date.now() + ext);
-        }
-    });
+  // MULTER PARAMS FOR FILES STORAGE
+  var storageFiles = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./uploads/files")
+    },
+    filename: function (req, file, cb) {
+      let ext = file.originalname.substring(
+        file.originalname.lastIndexOf("."),
+        file.originalname.length,
+      )
+      cb(null, file.originalname + "-" + Date.now() + ext)
+    },
+  })
 
+  var uploadFiles = multer({ storage: storageFiles })
 
-    var uploadFiles = multer({ storage: storageFiles });
+  var uploadImages = multer({ storage: storageImages })
 
-    var uploadImages = multer({ storage: storageImages });
+  // ******************************************************** BACK END CRUD CLOUDINARY IMAGE STORING WEB SERVICE ****************************************************** */
 
+  // CREATE A PICTURE ON CLOUDINARY
+  app.post("/images", uploadImages.single("file"), function (req, res, next) {
+    console.log(req.file)
 
+    // STEP 1 : reducing picture size
+    sharp(req.file.path)
+      .resize(200, 200)
+      .toBuffer(function (err, buffer) {
+        fs.writeFile(req.file.path, buffer, function (e) { })
+      })
 
-    // ******************************************************** BACK END CRUD WEB SERVICES ****************************************************** */  
+    // STEP 2 : CLOUDINARY STORING
+    // ALSO STORING THE PICTURE ON CLOUDINARY https://support.cloudinary.com/hc/en-us/articles/202520762-How-to-upload-images-while-keeping-their-original-filenames-
+    // {"use_filename": true, "unique_filename": false}, NOT WORKING
+    cloudinary.uploader.upload("./uploads/img/" + req.file.filename, function (result) {
+      console.log("moncloudinary")
+      console.log(result.url)
+      // SENDING THE CLOUDINARY URL FOR FRONT END DISPLAYING
+      res.send({ filename: result.url })
+    })
+    // SENDING THE UPLOADS URL FOR DISPLAYING
+    // res.send({ filename: req.file.filename});
+  })
 
-    
-    // STORING PICTURES ON CLOUDINARY
-    app.post("/images", uploadImages.single("file"), function(req, res, next) {
-        console.log(req.file);
-        
-        // STEP 1 : reducing picture size
-        sharp(req.file.path) 
-            .resize(200, 200)
-            .toBuffer(function(err, buffer) {
-                fs.writeFile(req.file.path, buffer, function(e) {});
-            });
+  // READ A PICTURE ON CLOUDINARY
+  app.post("/readImage", function (req, res, next) {
+    // TO DO
+  })
 
-          // STEP 2 : CLOUDINARY STORING
-          // ALSO STORING THE PICTURE ON CLOUDINARY https://support.cloudinary.com/hc/en-us/articles/202520762-How-to-upload-images-while-keeping-their-original-filenames-
-            // {"use_filename": true, "unique_filename": false}, NOT WORKING 
-          cloudinary.uploader.upload("./uploads/img/"+ req.file.filename,
-            function(result) { 
-              console.log('moncloudinary')
-              console.log(result.url) ;
-              // SENDING THE CLOUDINARY URL FOR FRONT END DISPLAYING
-              res.send({ filename: result.url});
-            })
-            // SENDING THE UPLOADS URL FOR DISPLAYING
-              // res.send({ filename: req.file.filename});
-    
-        // res.sendStatus(200);
-    });
+  // UPDATE A PICTURE ON CLOUDINARY
+  app.post("/updateImage", function (req, res, next) {
+    // TO DO
+  })
 
+  // DELETE A PICTURE ON CLOUDINARY
+  app.post("/deleteImage", function (req, res, next) {
+    // TO DO
+  })
 
-    // STORING FILES ON THE NODE SERVER
-    app.post("/createFiles", uploadFiles.array("file", 10), function(req, res, err) {
-        if (err) {
-            console.log(err);
-        }
-        var filenames = [];
-        req.files.forEach(function(file) {
-            console.log(file);
-            filenames.push(file);
-        });
-        res.send(filenames);
-    });
+  // ******************************************************** BACK END CRUD FTP IMAGE STORING WEB SERVICE ****************************************************** */
 
-    app.post("/deleteFile", function(req, res) {
-        // LOGGED IN CONTROL
-        if (!req.session.loggedIn) {
-            console.log(" FORBIDDEN ");
-            res.status(403).send({ errorCode: "403" });
-            return;
-        }
+  // CREATE A PICTURE ON FTP
+  app.post("/createFtpimage", function (req, res, next) {
+    // TO DO
+  })
 
-        // CONTROLE DE LA PERMISSION
-        if (!permissions.permission_valid("DELETE_FILE", req)) {
-            console.log(" NO RIGHTS");
-            res.status(403).send({ errorCode: "403" });
-            return;
-        }
-        console.log(req.body);
-        var file = req.body.name;
+  // READ A PICTURE ON FTP
+  app.post("/readFtpImage", function (req, res, next) {
+    // TO DO
+  })
 
-        try {
-            var fs = require("fs");
-            var filePath = "./uploads/files/" + file;
-            fs.unlinkSync(filePath);
+  // UPDATE A PICTURE ON FTP
+  app.post("/updateFtpImage", function (req, res, next) {
+    // TO DO
+  })
 
-            res.sendStatus(200);
-        } catch (e) {
-            console.log(e);
-            res.sendStatus(400);
-        }
+  // DELETE A PICTURE ON FTP
+  app.post("/deleteFtpImage", function (req, res, next) {
+    // TO DO
+  })
 
-        // -------------------------------------------------------- END CRUD 
-    });
-    // ----------------------------------- END UPLOADING IMAGES AND FILES -----------------------------------
-};
+  // ******************************************************** BACK END CRUD NODE SERVER FILES WEB SERVICE ****************************************************** */
+
+  // CREATE MULTIPLE FILES
+  app.post("/createFiles", uploadFiles.array("file", 10), function (req, res, err) {
+    if (err) {
+      console.log(err)
+    }
+    var filenames = []
+    req.files.forEach(function (file) {
+      console.log(file)
+      filenames.push(file)
+    })
+    res.send(filenames)
+  })
+
+  // READ A FILE ON FTP
+  app.post("/readFile", function (req, res, next) {
+    // TO DO
+  })
+
+  // UPDATE A FILE ON FTP
+  app.post("/updateFile", function (req, res, next) {
+    // TO DO
+  })
+
+  // DELETE A FILE
+  app.post(
+    "/deleteFile",
+    permissions.requiresLoggedIn,
+    permissions.permission_valid("DELETE_FILE"),
+    function (req, res) {
+      console.log(req.body)
+      var file = req.body.name
+
+      try {
+        var fs = require("fs")
+        var filePath = "./uploads/files/" + file
+        fs.unlinkSync(filePath)
+
+        res.sendStatus(200)
+      } catch (e) {
+        console.log(e)
+        res.sendStatus(400)
+      }
+    },
+  )
+
+  // ----------------------------------- END UPLOADING IMAGES AND FILES -----------------------------------
+}
