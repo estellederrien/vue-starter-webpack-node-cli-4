aff1457f6e6dab1a5f00ed6695619c96797cb617
NIcolas <nicolas.huleux@mail.com>
2020-09-03 14:30:54 +0200

    Fixing the TMP files path for heroku - Réparation du chemin pour stocker les fichiers temps

--- cruds/filesCrud.js
+++ cruds/filesCrud.js
@@ -13,109 +13,109 @@
 */
 
 module.exports = function(app, db, permissions) {
-	/* ************************************* NODE MODULES ****************************************************** */
+    /* ************************************* NODE MODULES ****************************************************** */
 
-	var cloudinary = require("cloudinary") // Managing the cloudinary server pictures storing
-	var Client = require("ssh2-sftp-client") // Managing the SFTP server connexion
-	var multer = require("multer") // Managing multiples files uploads
-	const sharp = require("sharp") // Resizing pictures module
-	var fs = require("fs") // Managing the file system
-	const bodyParser = require("body-parser")
+    var cloudinary = require("cloudinary") // Managing the cloudinary server pictures storing
+    var Client = require("ssh2-sftp-client") // Managing the SFTP server connexion
+    var multer = require("multer") // Managing multiples files uploads
+    const sharp = require("sharp") // Resizing pictures module
+    var fs = require("fs") // Managing the file system
+    const bodyParser = require("body-parser")
 
-	/* ************************************* STORAGE SERVERS CONNEXIONS PARAMS ****************************************************** */
+    /* ************************************* STORAGE SERVERS CONNEXIONS PARAMS ****************************************************** */
 
-	/*  CLOUDINARY PARAMS 'STORING PICTURES ON THE CLOUD' YOU HAVE TO OWN A CLOUDINARY ACCOUNT */
+    /*  CLOUDINARY PARAMS 'STORING PICTURES ON THE CLOUD' YOU HAVE TO OWN A CLOUDINARY ACCOUNT */
 
-	cloudinary.config({
-		cloud_name: "ddq5asuy2",
-		api_key: "354237299578646",
-		api_secret: "3UWkrND91MW3jhmGecvp77uetvQ",
-	})
+    cloudinary.config({
+        cloud_name: "ddq5asuy2",
+        api_key: "354237299578646",
+        api_secret: "3UWkrND91MW3jhmGecvp77uetvQ",
+    })
 
-	/* 
+    /* 
   FTP SERVER PARAMS 'STORING FILES AND PICTURES ON MY PERSONNAL FTP'  YOU HAVE TO OWN A FTP SERVER
 */
-	let sftp = new Client()
+    let sftp = new Client()
 
-	sftp.connect({
-		host: "test.rebex.net",
-		user: "demo",
-		password: "password",
-		port: 22,
-	})
-		.then(() => {
-			return sftp.list("/")
-		})
-		.then((data) => {
-			console.log(data, "the data info")
-		})
-		.catch((err) => {
-			console.log(err, "catch error")
-		})
+    sftp.connect({
+            host: "test.rebex.net",
+            user: "demo",
+            password: "password",
+            port: 22,
+        })
+        .then(() => {
+            return sftp.list("/")
+        })
+        .then((data) => {
+            console.log(data, "the data info")
+        })
+        .catch((err) => {
+            console.log(err, "catch error")
+        })
 
-	/* ************************************* MULTER FILE HANDLINGS ****************************************************** */
+    /* ************************************* MULTER FILE HANDLINGS ****************************************************** */
 
-	// MULTER PARAMS FOR FILES STORAGE
-	var storageFiles = multer.diskStorage({
-		destination: function(req, file, cb) {
-			cb(null, "./uploads/files")
-		},
-		filename: function(req, file, cb) {
-			let ext = file.originalname.substring(
-				file.originalname.lastIndexOf("."),
-				file.originalname.length,
-			)
-			cb(null, file.originalname + "-" + Date.now() + ext)
-		},
-	})
+    // MULTER PARAMS FOR FILES STORAGE
+    var storageFiles = multer.diskStorage({
+        destination: function(req, file, cb) {
+            cb(null, "./tmp/files")
+        },
+        filename: function(req, file, cb) {
+            let ext = file.originalname.substring(
+                file.originalname.lastIndexOf("."),
+                file.originalname.length,
+            )
+            cb(null, file.originalname + "-" + Date.now() + ext)
+        },
+    })
 
-	var uploadFiles = multer({ storage: storageFiles })
+    var uploadFiles = multer({ storage: storageFiles })
 
-	// ******************************************************** BACK END CRUD NODE SERVER FILES WEB SERVICE ****************************************************** */
+    // ******************************************************** BACK END CRUD NODE SERVER FILES WEB SERVICE ****************************************************** */
 
-	// CREATE MULTIPLE FILES
-	app.post("/createFiles", uploadFiles.array("file", 10), function(req, res, err) {
-		if (err) {
-			console.log(err)
-		}
-		var filenames = []
-		req.files.forEach(function(file) {
-			console.log(file)
-			filenames.push(file)
-		})
-		res.send(filenames)
-	})
+    // CREATE MULTIPLE FILES
+    app.post("/createFiles", uploadFiles.array("file", 10), function(req, res, err) {
+        if (err) {
+            console.log(err)
+        }
+        var filenames = []
+        req.files.forEach(function(file) {
+            console.log(file)
+            filenames.push(file)
+        })
+        res.send(filenames)
+    })
 
-	// READ A FILE ON FTP
-	app.post("/readFile", function(req, res, next) {
-		// TO DO
-	})
+    // READ A FILE ON FTP
+    app.post("/readFile", function(req, res, next) {
+        // TO DO
+    })
 
-	// UPDATE A FILE ON FTP
-	app.post("/updateFile", function(req, res, next) {
-		// TO DO
-	})
+    // UPDATE A FILE ON FTP
+    app.post("/updateFile", function(req, res, next) {
+        // TO DO
+    })
 
-	// DELETE A FILE
-	app.post("/deleteFile",
-		permissions.requiresLoggedIn,
-		permissions.permission_valid("DELETE_FILE"),
-		function(req, res) {
-			console.log(req.body)
-			var file = req.body.name
+    // DELETE A FILE
+    app.post("/deleteFile",
+        permissions.requiresLoggedIn,
+        permissions.permission_valid("DELETE_FILE"),
+        function(req, res) {
+            console.log(req.body)
+            var file = req.body.name
 
-			try {
-				var fs = require("fs")
-				var filePath = "./uploads/files/" + file
-				fs.unlinkSync(filePath)
+            try {
+                var fs = require("fs")
+                var filePath = "./tmp/files/" + file
+                fs.unlinkSync(filePath)
 
-				res.sendStatus(200)
-			} catch (e) {
-				console.log(e)
-				res.sendStatus(400)
-			}
-		},
-	)
+                res.sendStatus(200)
+            } catch (e) {
+                console.log(e)
+                res.sendStatus(400)
+            }
+        },
+    )
 
-	// ----------------------------------- END UPLOADING IMAGES AND FILES -----------------------------------
+    // ----------------------------------- END UPLOADING IMAGES AND FILES -----------------------------------
 }
