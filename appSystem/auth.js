378746b324a0ac736b2b80c229971265841ea826
nicolas <nicolas.huleux@mail.com>
2020-09-16 16:20:21 +0200

    Fixing ugly Auth back end code !

--- appSystem/auth.js
+++ appSystem/auth.js
@@ -1,43 +1,42 @@
 // ----------------------------------- MONGODB AUTH USER  -------------------------------------------
 module.exports = function(app, db, session, bcrypt) {
- 
   /*
-     * Auth a user on MONGODB ATLAS
-     * @params JSON OBJECT 
-     * @return Status 200
-     * @error  Status 400
-     */
-  
-  app.post("/getAuth", function(req, res) {
+   * Auth a user on MONGODB ATLAS
+   * @params JSON OBJECT
+   * @return Status 200
+   * @error  Status 400
+   */
 
-    // 1. Getting front end DATA - On récupère log in et mot de passe du front end
+  app.post("/getAuth", function(req, res) {
+    // step 1. Getting front end DATA (email and password) - On récupère log in et mot de passe du front end
     var user = req.body;
 
-    // 2. FINDING EMAIL IN DATABASE - ON cherche l'email dans la db
-    db.collection("users").findOne({ email: user.email }, function(
-      findErr,
-      result
-    ) {
+    // step 2. FINDING EMAIL IN DATABASE - ON cherche l'email dans la db
+    db.collection("users").findOne({ email: user.email }, function(findErr,result) {
+      
       if (findErr) res.status(403).send({ errorCode: "403" });
 
       if (!result) {
+        console.log("USER not present in the DATABASE");
         res.status(403).send({ errorCode: "403" });
       } else {
-        // 3. COMPARING PASSWORDS  - ON compare le password entré et celui de la database
-        if (bcrypt.compareSync(user.password, result.password)) {
-          // Passwords match
-
+        
+        // step 3. sCOMPARING PASSWORDS  - ON compare le password entré et celui de la database
+        if (bcrypt.compareSync(user.password, result.password)) {  // Passwords match
+        
+          // USER ALREADY HAS A BACK END SESSION ID
           console.log("reqsessionId ; " + req.sessionID);
 
-          // 4. BUILDING THE BACKEND USER SESSION - AJOUT DES DATAS DU USER A LA SESSION ET LE FAIT QUIL EST LOGGE
+          // step 4. BUILDING THE BACKEND USER SESSION - AJOUT DES DATAS DU USER A LA SESSION ET LE FAIT QUIL EST LOGGE
           req.session.loggedIn = true;
           req.session.user = result;
 
-          // 5. SENDING USER DATA TO FRONT END ENVOI DU USER AU FRONT END
+          // step 5. SENDING USER DATA TO FRONT END ENVOI DU USER AU FRONT END
           res.send(result);
-          
+
         } else {
-          // Passwords don't match 6 Erreur de mot de passe
+          // 
+          // step 6 : Passwords don't match  Erreur de mot de passe
           console.log("Passwords don't match");
           res.status(403).send({ errorCode: "403" });
         }
@@ -45,19 +44,18 @@ module.exports = function(app, db, session, bcrypt) {
     });
   });
 
-    /*
-     * Loggin out a user on MONGODB ATLAS - Delogge utilisateur sur mongodb atlas
-     * @params JSON OBJECT 
-     * @return Status 200
-     * @error  Status 400
-     */
+  /*
+   * Loggin out an APP user  - Delogge un utilisateur 
+   * @params NONE
+   * @return
+   * @error  
+   */
 
   app.post("/logout", function(req, res) {
     req.session.loggedIn = false;
     console.log("NOT LOGGED ANY MORE");
     res.sendStatus(200);
   });
-
 
   // -----------------------------------FIN AUTH USER-------------------------------------------
 };
