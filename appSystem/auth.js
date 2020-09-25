b2a3c88723eb681b2bb75a48eead375eec29f86e
NIcolas <nicolas.huleux@mail.com>
2020-09-26 00:09:15 +0200

    Setting up the Logs system in logs.txt - Installation du système de logs dans le fichier logs.txt

--- appSystem/auth.js
+++ appSystem/auth.js
@@ -1,61 +1,66 @@
 // ----------------------------------- AUTHENTICATE USER  -------------------------------------------
-module.exports = function(app, db, session, bcrypt) {
-  /*
-   * Authenticate a MONGODB ATLAS user  - Authentifier un utilisateur sur Mongodb ATLAS
-   * @params JSON OBJECT - EXAMPLE : {"email":"urEmail","password":"urPassword"}
-   * @return JSON OBJECT - dbUser
-   * @error  Status 403
-   */
+module.exports = function(app, db, session, bcrypt, logStream) {
+    /*
+     * Authenticate a MONGODB ATLAS user  - Authentifier un utilisateur sur Mongodb ATLAS
+     * @params JSON OBJECT - EXAMPLE : {"email":"urEmail","password":"urPassword"}
+     * @return JSON OBJECT - dbUser
+     * @error  Status 403
+     */
 
-  app.post("/getAuth", function(req, res) {
-    // AN ANONYMOUS USER ALREADY HAS A BACK END SESSION ID, WE LOOK AT IT - Un utilisateur anonyme possède déjà un id de session , gràce au module node session
-    console.log("reqsessionId ; " + req.sessionID);
+    app.post("/getAuth", function(req, res) {
+        // AN ANONYMOUS USER ALREADY HAS A BACK END SESSION ID, WE LOOK AT IT - Un utilisateur anonyme possède déjà un id de session , gràce au module node session
+        console.log("reqsessionId ; " + req.sessionID);
 
-    // step 1. Getting front end DATA (email and password) - On récupère log in et mot de passe du front end
-    var frontEndUser = req.body;
+        // step 1. Getting front end DATA (email and password) - On récupère log in et mot de passe du front end
+        var frontEndUser = req.body;
 
-    // step 2. FINDING EMAIL IN DATABASE - ON cherche l'email dans la db
-    db.collection("users").findOne({ email: frontEndUser.email }, function(findErr,dbUser) {
-      
-      if (findErr) res.status(403).send({ errorCode: "403" });
+        // step 2. FINDING EMAIL IN DATABASE - ON cherche l'email dans la db
+        db.collection("users").findOne({ email: frontEndUser.email }, function(findErr, dbUser) {
 
-      if (!dbUser) {
-        console.log("USER is not present in the DATABASE");
-        res.status(403).send({ errorCode: "403" });
-      } else {
-        // step 3. COMPARING PASSWORDS ( front end vs db ) - ON compare le password entré et celui de la database
-        if (bcrypt.compareSync(frontEndUser.password, dbUser.password)) { // Passwords match
-         
+            if (findErr) res.status(403).send({ errorCode: "403" });
 
-          // step 4. BUILDING THE BACKEND USER SESSION - AJOUT DES DATAS DU USER A LA SESSION ET LE FAIT QUIL EST LOGGE
-          req.session.loggedIn = true;
-          req.session.user = dbUser;
+            if (!dbUser) {
+                console.log("USER is not present in the DATABASE");
+                res.status(403).send({ errorCode: "403" });
+            } else {
+                // step 3. COMPARING PASSWORDS ( front end vs db ) - ON compare le password entré et celui de la database
+                if (bcrypt.compareSync(frontEndUser.password, dbUser.password)) { // Passwords match
 
-          // step 5. SENDING USER DATA TO FRONT END ENVOI DU USER AU FRONT END
-          res.send(dbUser);
 
-        } else {
-          //
-          // step 6 : Passwords don't match  Erreur de mot de passe
-          console.log("Passwords don't match");
-          res.status(403).send({ errorCode: "403" });
-        }
-      }
+                    // step 4. BUILDING THE BACKEND USER SESSION - AJOUT DES DATAS DU USER A LA SESSION ET LE FAIT QUIL EST LOGGE
+                    req.session.loggedIn = true;
+                    req.session.user = dbUser;
+
+                    // step 5. SENDING USER DATA TO FRONT END ENVOI DU USER AU FRONT END
+                    res.send(dbUser);
+
+                    // USER CONNEXION IS WRITTEN IN LOGS
+                    var d = new Date(Date.now());
+                    logStream.write('USER : ' + req.session.user.email + ' CONNECTED AT : ' + d.toString() + '\r\n');
+                    // logStream.end('\r\n');
+
+                } else {
+                    //
+                    // step 6 : Passwords don't match  Erreur de mot de passe
+                    console.log("Passwords don't match");
+                    res.status(403).send({ errorCode: "403" });
+                }
+            }
+        });
     });
-  });
 
-  /*
-   * Loggin out an APP user  - Delogge un utilisateur
-   * @params NONE
-   * @return 200
-   * @error NONE
-   */
+    /*
+     * Loggin out an APP user  - Delogge un utilisateur
+     * @params NONE
+     * @return 200
+     * @error NONE
+     */
 
-  app.post("/logout", function(req, res) {
-    req.session.loggedIn = false;
-    console.log("NOT LOGGED ANY MORE");
-    res.sendStatus(200);
-  });
+    app.post("/logout", function(req, res) {
+        req.session.loggedIn = false;
+        console.log("NOT LOGGED ANY MORE");
+        res.sendStatus(200);
+    });
 
-  // -----------------------------------FIN AUTH USER-------------------------------------------
+    // -----------------------------------FIN AUTH USER-------------------------------------------
 };
