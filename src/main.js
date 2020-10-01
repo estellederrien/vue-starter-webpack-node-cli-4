0ba9da916f48fad883c9978d3ab4b55378a1fd56
NIcolas <nicolas.huleux@mail.com>
2020-10-01 19:35:46 +0200

    Adding VUEX - Part 1 : setting up the store , setting up the USER object who will be coming from MONGODB  . Ajout de VUEX pour partager des variables entre les components, et en particulier l'objet UTILISATEUR, qui provient d'une requête MONGODB !

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 
 import App from "./App.vue";
 import router from "./router";
+import Vuex from "vuex";
 
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -24,25 +25,55 @@ Vue.use(Vuelidate);
 
 import Notifications from "vue-notification";
 import velocity from "velocity-animate";
-Vue.use(Notifications, { velocity });
 
-Vue.component("multiselect", Multiselect);
 import Multiselect from "vue-multiselect";
 
 import VueLazyload from 'vue-lazyload'
 
 Vue.use(VueLazyload)
+Vue.use(Notifications, { velocity });
 
+Vue.component("multiselect", Multiselect);
+// ---------------------------------------------- VUE LAZY LOAD -----------------------------------------------------------------------------------
 // or with options
 Vue.use(VueLazyload, {
-    preLoad: 1.3,
-    error: '../assets/img/error.png',
-    loading: '../assets/img/loader_gif.gif',
-    attempt: 1
-})
+        preLoad: 1.3,
+        error: '../assets/img/error.png',
+        loading: '../assets/img/loader_gif.gif',
+        attempt: 1
+    })
+    // ------------------------------------------ END VUE LAZY LOAD ---------------------------------------------------------------------------
 
-// ---------------------------------------------- STORING USER AS GLOBAL, THIS VAR IS POPULATED WHEN A USER LOGS IN INSIDE THE LOGIN COMPONENT for the PROFILE MENU TO APPEAR IN THE TOPBAR
-// ---------------------------------------------- VUEX WILL BE BETTER LATER
+
+// ---------------------------------------------- VUEX DATA STORE - SHARING DATA BETWEEN COMPONENTS !-----------------------------------------------
+// ---------------------------------------------- VUEX MAGASIN DE DATA - PARTAGER DES DATAS ENTRE LES COMPONENTS -------------------------------------
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    state: {
+        user: {}
+    },
+    mutations: {
+        setUser(state, user) {
+            state.user = user;
+        }
+    },
+    getters: {
+        user: state => {
+            return state.user
+        }
+    }
+});
+
+// ---------------------------------------------- END VUEX DATA STORE - SHARING DATA BETWEEN COMPONENTS !-------------------------------------
+
+
+
+
+
+
+// ---------------------------------------------- !!! OLD CODE !!!! STORING USER AS GLOBAL, THIS VAR IS POPULATED WHEN A USER LOGS IN INSIDE THE LOGIN COMPONENT for the PROFILE MENU TO APPEAR IN THE TOPBAR
+
 
 let globalData = new Vue({
     data: { $user: {} }
@@ -64,9 +95,11 @@ Vue.mixin({
         }
     }
 });
+// ---------------------------------------------- END !!! OLD CODE !!!! STORING USER AS GLOBAL, THIS VAR IS POPULATED WHEN A USER LOGS IN INSIDE THE LOGIN COMPONENT for the PROFILE MENU TO APPEAR IN THE TOPBAR
 
 
 new Vue({
     router,
-    render: h => h(App)
+    render: h => h(App),
+    store: store
 }).$mount("#app");
