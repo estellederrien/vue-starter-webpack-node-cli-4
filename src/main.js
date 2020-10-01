2c169758cb8d58defda1f025d942a3820dacf803
NIcolas <nicolas.huleux@mail.com>
2020-10-01 19:33:37 +0200

    Adding VUEX - Part 1 : setting up the store , setting up the USER object who will be coming from MONGODB  . Ajout de VUEX pour partager des variables entre les components, et en particulier l'objet UTILISATEUR, qui provient d'une requête MONGODB !

---
+++ src/main.js
@@ -0,0 +1,72 @@
+import Vue from "vue";
+
+import App from "./App.vue";
+import router from "./router";
+
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import VueSidebarMenu from "vue-sidebar-menu";
+import "vue-sidebar-menu/dist/vue-sidebar-menu.css";
+
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
+
+Vue.use(VueSidebarMenu);
+Vue.config.productionTip = false;
+
+// https://github.com/euvl/vue-js-modal
+import VModal from "vue-js-modal";
+Vue.use(VModal, { dynamic: true });
+
+import Vuelidate from "vuelidate";
+Vue.use(Vuelidate);
+
+import Notifications from "vue-notification";
+import velocity from "velocity-animate";
+Vue.use(Notifications, { velocity });
+
+Vue.component("multiselect", Multiselect);
+import Multiselect from "vue-multiselect";
+
+import VueLazyload from 'vue-lazyload'
+
+Vue.use(VueLazyload)
+
+// or with options
+Vue.use(VueLazyload, {
+    preLoad: 1.3,
+    error: '../assets/img/error.png',
+    loading: '../assets/img/loader_gif.gif',
+    attempt: 1
+})
+
+// ---------------------------------------------- STORING USER AS GLOBAL, THIS VAR IS POPULATED WHEN A USER LOGS IN INSIDE THE LOGIN COMPONENT for the PROFILE MENU TO APPEAR IN THE TOPBAR
+// ---------------------------------------------- VUEX WILL BE BETTER LATER
+
+let globalData = new Vue({
+    data: { $user: {} }
+});
+
+
+Vue.mixin({
+    computed: {
+        $user: {
+            get: function() {
+
+                return globalData.$data.$user;
+
+            },
+            set: function(user) {
+
+                globalData.$data.$user = user;
+            }
+        }
+    }
+});
+
+
+new Vue({
+    router,
+    render: h => h(App)
+}).$mount("#app");
