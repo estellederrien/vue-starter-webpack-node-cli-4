53dd16c5cbd544fcd531b3468b9cae9ddb8f5fc3
NIcolas <nicolas.huleux@mail.com>
2020-10-06 19:09:17 +0200

    Adding Mongoose Job schema , no constraints for now - On ajoute le schéma mongoose job pour l'instant pas de contraintes

---
+++ models/message.js
@@ -0,0 +1,20 @@
+// LOADING LIBS
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var ObjectId = require('mongodb').ObjectID;
+
+// MESSAGE SCHEMA
+const MessageSchema = new Schema({
+    _id: ObjectId,
+    to: String,
+    from: String,
+    content: String,
+    senderName: String,
+    senderSurname: String,
+    date: String,
+    id: String,
+    img: String,
+    date: String
+});
+
+module.exports = mongoose.model('Message', Messagechema)
