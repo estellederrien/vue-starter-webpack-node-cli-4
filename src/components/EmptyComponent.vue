c3143f434eec9c03a8b5b1e888db01c74c050a0c
NIcolas <nicolas.huleux@mail.com>
2020-10-18 11:29:15 +0200

    Adding a standard empty component for faster components devs . Ajout d'un component vide standard pour faciliter les développements.

---
+++ src/components/EmptyComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        Empty component
+    </div>
+</template>
+
+<script>
+// Import others components
+import axios from "axios";
+
+export default {
+    name: "emptycomponent",
+    data() {
+        return {
+            var1 : "Test"
+        };
+    },
+    methods: {
+
+    },
+    beforeCreate: function () {
+
+    },
+    mounted: function () {
+
+    }
+};
+</script>
+
+<style scoped>
+</style>
