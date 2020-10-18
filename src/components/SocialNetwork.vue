a7c6490457c07677e6c06242710082f712101074
NIcolas <nicolas.huleux@mail.com>
2020-10-18 11:41:16 +0200

    Setting up the social networking module PART 1 : Front end : Crud functions , and data modeling. On construit le module de réseau social front end : Les fonctions CRUD et le format de data ( 1 objet json et un array)

---
+++ src/components/SocialNetwork.vue
@@ -0,0 +1,48 @@
+<template>
+<div>
+    social network
+</div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    name: "socialnetwork",
+    data() {
+        return {
+            // This JSON object must be equals to the Moongoose backend modeled object - Cet objet json doit correspondre à l'objet défini dans le répertoire MODELS (moongoose) de l'app
+            new_social_message:{
+                from:"",
+                to:"",
+                date:"",
+                content:"",
+                title:"",
+                user_img:""
+            },
+            // Social message list will appears, 5 by five or using an infinite scroll - La liste des messages est un array de json.
+            social_messages:[]
+        };
+    },
+
+    methods: {
+        // Social message crud functions
+        create_social_message(){
+
+        },
+        read_social_message(){
+
+        },
+        update_social_message(){
+
+        },
+        delete_social_message(){
+
+        }
+    },
+};
+</script>
+
+<style scoped>
+
+</style>
