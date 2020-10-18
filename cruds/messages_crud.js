939f37755b18b1e88df582defb015652188c4f2f
NIcolas <nicolas.huleux@mail.com>
2020-10-18 12:19:11 +0200

    Normalizing crud using mongoose - On refactorise le code grace à mongoose .

--- cruds/messages_crud.js
+++ cruds/messages_crud.js
@@ -1,29 +1,30 @@
 // ---------------------------------- MESSAGES CRUD -------------------------------------------
 module.exports = function(app, db, Message, ObjectId) {
 
-    /* MESSAGE MODEL*/
-    let message = {
-        _id: "",
-        to: "",
-        from: "",
-        content: "",
-        senderName: "",
-        senderSurname: "",
-        date: "",
-        id: "",
-        img: "",
-        date: ""
-    }
-
     /*
      * Creating a message
+     * @params JSON OBJECT : {}
      * @return Status 200
      * @error  Status 400
      */
     app.post("/createMessage", function(req, res, next) {
-        var message = req.body;
-        message.date = new Date();
+
+        // 1.  Getting data from front end - on récupère le json data de l'app vue.js
+        var new_message = req.body;
+
         try {
+            var message = new Message({
+                _id: null, // It is a new message it has no mongodb id yet - Cest un nouveau message, il n'y a pas encore d'id mongodb, il se cree lors de la phase d'insert
+                to: new_message.to,
+                from: new_message.from,
+                content: new_message.content,
+                senderName: new_message.senderName,
+                senderSurname: new_message.senderSurname,
+                date: new_message.date,
+                id: new_message.id,
+                img: new_message.img,
+                date: new_message.date
+            });
             db.collection("messages").insertOne(message);
             res.sendStatus(200);
         } catch (e) {
@@ -34,6 +35,7 @@ module.exports = function(app, db, Message, ObjectId) {
 
     /*
      * Read user's messages
+     * @params JSON OBJECT : {}
      * @return JSON ARRAY
      * @error  
      */
