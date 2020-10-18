5e0571950245f17525475acd88ec19f19fd7c85c
NIcolas <nicolas.huleux@mail.com>
2020-10-18 13:13:27 +0200

    Adding empty component infos

--- src/components/EmptyComponent.vue
+++ src/components/EmptyComponent.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-        Empty component
-    </div>
+<div>
+    Empty component
+</div>
 </template>
 
 <script>
@@ -10,15 +10,16 @@ import axios from "axios";
 
 export default {
     name: "emptycomponent",
+    props: ["_id"], // Use Props variables like this : In vue router  path add for example the _id var : "/user/:_id", then in the initial module, call like that :  this.$router.push("/user/" + _id);
+    beforeCreate: function () {
+
+    },
     data() {
         return {
-            var1 : "Test"
+            var1: "Test"
         };
     },
     methods: {
-
-    },
-    beforeCreate: function () {
 
     },
     mounted: function () {
