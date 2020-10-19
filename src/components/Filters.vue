aa2e2577e515fbc5aee8e47a3c0bbfcba2676565
NIcolas <nicolas.huleux@mail.com>
2020-10-19 20:11:25 +0200

    Fix jobs filters using the new generic crud

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -1,104 +1,78 @@
 <template>
-        <div class="card mb-3 text-center">
-                <div class="card-header">
-                        <label style="padding:5px">
-                                <!-- test -->
-                                <b> <i class="fas fa-filter"></i> Filtres </b>
-                        </label>
-                        <button @click="closeModal" class="btn btn-primary float-right">X</button>
+<div class="card mb-3 text-center">
+    <div class="card-header">
+        <label style="padding:5px">
+            <!-- test -->
+            <b> <i class="fas fa-filter"></i> Filtres </b>
+        </label>
+        <button @click="closeModal" class="btn btn-primary float-right">X</button>
+    </div>
+    <div class="card-body d-flex flex-column">
+        <div class="row">
+            <div class="col-md-6" style="padding:30px">
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <label class="input-group-text">Role</label>
+                    </div>
+                    <select class="form-control" v-model="modifiedFilters.role">
+                        <option value>Choisir</option>
+                        <option value="viewer">Viewer</option>
+                        <option value="user">User</option>
+                        <option value="manager">Manager</option>
+                    </select>
                 </div>
-                <div class="card-body d-flex flex-column">
-                        <div class="row">
-                                <div class="col-md-6" style="padding:30px">
-                                        <div class="input-group mb-3">
-                                                <div class="input-group-prepend">
-                                                        <label class="input-group-text">Role</label>
-                                                </div>
-                                                <select class="form-control" v-model="modifiedFilters.role">
-                                                        <option value>Choisir</option>
-                                                        <option value="viewer">Viewer</option>
-                                                        <option value="user">User</option>
-                                                        <option value="manager">Manager</option>
-                                                </select>
-                                        </div>
-                                        <br />
-                                        <div class="input-group mb-3">
-                                                <div class="input-group mb-3">
-                                                        <div class="input-group-prepend">
-                                                                <label class="input-group-text">Jobs</label>
-                                                        </div>
-                                                        <multiselect
-                                                                class="form-control"
-                                                                v-model="modifiedFilters.jobs"
-                                                                :multiple="true"
-                                                                :options="jobs"
-                                                                :searchable="true"
-                                                                :close-on-select="true"
-                                                                :show-labels="false"
-                                                                placeholder="Choix multiple"
-                                                        ></multiselect>
-                                                        <pre class="language-json"><code>{{ value }}</code></pre>
-                                                </div>
-                                                <br />
-                                        </div>
-                                         <div class="input-group mb-3">
-                                                <div class="input-group-prepend">
-                                                        <label class="input-group-text">Groupes</label>
-                                                </div>
-                                                <multiselect
-                                                        class="form-control"
-                                                        v-model="modifiedFilters.groups"
-                                                        :multiple="true"
-                                                        :options="groups"
-                                                        :searchable="true"
-                                                        :close-on-select="true"
-                                                        :show-labels="false"
-                                                        placeholder="Choix multiple"
-                                                ></multiselect>
-                                                <pre class="language-json"><code>{{ value }}</code></pre>
-                                        </div>
-                                        <br />
-                                </div>
-                                <div class="col-md-6" style="padding:30px">
-                                        <div class="input-group mb-3">
-                                                <div class="input-group-prepend">
-                                                        <label class="input-group-text">Utilisateur</label>
-                                                </div>
-                                                <multiselect
-                                                        class="form-control"
-                                                        v-model="modifiedFilters.users"
-                                                        :multiple="true"
-                                                        :options="users"
-                                                        :searchable="true"
-                                                        :close-on-select="true"
-                                                        :show-labels="false"
-                                                        placeholder="Choix multiple"
-                                                ></multiselect>
-                                                <pre class="language-json"><code>{{ value }}</code></pre>
-                                        </div>
-                                        <br />
-                                        <div class="mb-3">
-                                                <div>
-                                                        <div class="input-group-prepend">
-                                                                <label class="input-group-text">Age</label>
-                                                        </div>
-                                                        <vue-slider v-model="modifiedFilters.ageValues" :tooltip="'always'" :enable-cross="false"></vue-slider>
-                                                </div>
-                                        </div>
-                                </div>
-                       
+                <br />
+                <div class="input-group mb-3">
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <label class="input-group-text">Jobs</label>
                         </div>
-                        <div class="card-footer float-right">
-                                <!-- LOADING PIC -->
-                                <button class="btn btn-warning float-right mt-auto" v-on:click="filterNow()">
-                                        <i class="fas fa-filter"></i> Filtrer !
-                                        <img v-if="loading" style="width:30px" src="../assets/img/loader_def.gif" />
-                                </button>
-                                <button class="btn btn-primary float-right mt-auto" v-on:click="initializeFilters()">Initialiser</button>
-                        </div>
+                        <multiselect class="form-control" v-model="modifiedFilters.jobs" :multiple="true" :options="jobs" :searchable="true" :close-on-select="true" :show-labels="false" placeholder="Choix multiple"></multiselect>
+                        <pre class="language-json"><code>{{ value}}</code></pre>
+                    </div>
+                    <br />
                 </div>
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <label class="input-group-text">Groupes</label>
+                    </div>
+                    <multiselect class="form-control" v-model="modifiedFilters.groups" :multiple="true" :options="groups" :searchable="true" :close-on-select="true" :show-labels="false" placeholder="Choix multiple"></multiselect>
+                    <pre class="language-json"><code>{{ value }}</code></pre>
+                </div>
+                <br />
+            </div>
+            <div class="col-md-6" style="padding:30px">
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <label class="input-group-text">Utilisateur</label>
+                    </div>
+                    <multiselect class="form-control" v-model="modifiedFilters.users" :multiple="true" :options="users" :searchable="true" :close-on-select="true" :show-labels="false" placeholder="Choix multiple"></multiselect>
+                    <pre class="language-json"><code>{{ value }}</code></pre>
+                </div>
+                <br />
+                <div class="mb-3">
+                    <div>
+                        <div class="input-group-prepend">
+                            <label class="input-group-text">Age</label>
+                        </div>
+                        <vue-slider v-model="modifiedFilters.ageValues" :tooltip="'always'" :enable-cross="false"></vue-slider>
+                    </div>
+                </div>
+            </div>
+
         </div>
+        <div class="card-footer float-right">
+            <!-- LOADING PIC -->
+            <button class="btn btn-warning float-right mt-auto" v-on:click="filterNow()">
+                <i class="fas fa-filter"></i> Filtrer !
+                <img v-if="loading" style="width:30px" src="../assets/img/loader_def.gif" />
+            </button>
+            <button class="btn btn-primary float-right mt-auto" v-on:click="initializeFilters()">Initialiser</button>
+        </div>
+    </div>
+</div>
 </template>
+
 <script>
 /* GITHUB COMPONENTS */
 import axios from "axios";
@@ -111,105 +85,114 @@ import Multiselect from "vue-multiselect";
 import VueSlider from "vue-slider-component";
 import "vue-slider-component/theme/default.css";
 export default {
-        name: "filters",
-        props: ["loading"],
-        data() {
-                return {
-                        jobs: [],
-                        users: [],
-                        groups: [],
-                        modifiedFilters: {
-                                ageValues: [18, 60],
-                                role: "",
-                                jobs: [],
-                                users: [],
-                                groups:[]
-                        },
-                        value: "",
-                        /* Age range filter */
-                        min: 0,
-                        max: 100,
-                        enableCross: false,
-                        tooltipMerge: false,
-                        formatter: (value) => `Age : ${value}`,
-                };
+    name: "filters",
+    props: ["loading"],
+    data() {
+        return {
+            jobs: [],
+            users: [],
+            groups: [],
+            modifiedFilters: {
+                ageValues: [18, 60],
+                role: "",
+                jobs: [],
+                users: [],
+                groups: []
+            },
+            value: "",
+            /* Age range filter */
+            min: 0,
+            max: 100,
+            enableCross: false,
+            tooltipMerge: false,
+            formatter: (value) => `Age : ${value}`,
+        };
+    },
+    components: {
+        Multiselect,
+        // VueRangeSlider,
+        VueSlider,
+    },
+    methods: {
+        readJobsForFilters: function () {
+            axios
+                .get("/api/jobs")
+                .then(response => {
+                    this.jobs = response.data;
+                      this.jobs = this.jobs.map(function(item) {
+                        return item["name"];
+                        });
+                        
+                })
+                .catch(error => {
+                    console.log(error);
+
+                });
         },
-        components: {
-                Multiselect,
-                // VueRangeSlider,
-                VueSlider,
+        readGroupsForFilters: function () {
+            axios.post("/readGroupsForFilters")
+                .then((response) => {
+                    this.groups = response.data;
+
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
         },
-        methods: {
-                readJobsForFilters: function() {
-                        axios.post("/readJobsForFilters")
-                                .then((response) => {
-                                        this.jobs = response.data;
-                                })
-                                .catch(function(error) {
-                                        console.log(error);
-                                });
-                },
-                readGroupsForFilters: function() {
-                        axios.post("/readGroupsForFilters")
-                                .then((response) => {
-                                        this.groups = response.data;
-                                       
-                                })
-                                .catch(function(error) {
-                                        console.log(error);
-                                });
-                },
-                readUsers: function() {
-                        axios.post("/readUsersForFilters")
-                                .then((response) => {
-                                        this.users = response.data;
-                                })
-                                .catch(function(error) {
-                                        console.log(error);
-                                });
-                },
-                filterNow: function() {
-                        /*   Executing the executeFilters parent function with modifiedFilters as parameters , 
-                        the executefilters function needs to be called from the component call inside of the parent :  
-                        <filters  @filters="executeFilters" ></filters> */
-                        // Displaying loader
-                        this.loading = true;
-                        // Storing filters in localStorage, for later displaying - On enregistre les filtres que lutilisateur a choisi dans le localstorage
-                        localStorage.setItem('filters', JSON.stringify(this.modifiedFilters));
-                        // Sending filters to the parent component
-                        this.$emit("filters", this.modifiedFilters);
-                },
-                initializeFilters: function() {
-                        this.modifiedFilters = {
-                                ageValues: [18, 60],
-                        };
-                       localStorage.removeItem('filters');
-                        this.$emit("filters", this.modifiedFilters);
-                },
-                closeModal: function() {
-                        this.$modal.hide("filters");
-                },
+        readUsers: function () {
+            axios.post("/readUsersForFilters")
+                .then((response) => {
+                    this.users = response.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
         },
-        mounted: function() {
-                this.readJobsForFilters();
-                this.readUsers();
-                this.readGroupsForFilters();
-                 //Getting filters from localStorage, if they do exist - On va chercher les filtrs dans le local storage si ils ont été utlisés auparavant.
-               if(localStorage.getItem('filters')){
-                    this.modifiedFilters = JSON.parse(localStorage.getItem('filters'));
-                } 
-               
+        filterNow: function () {
+            /*   Executing the executeFilters parent function with modifiedFilters as parameters , 
+            the executefilters function needs to be called from the component call inside of the parent :  
+            <filters  @filters="executeFilters" ></filters> */
+            // Displaying loader
+            this.loading = true;
+            // Storing filters in localStorage, for later displaying - On enregistre les filtres que lutilisateur a choisi dans le localstorage
+            localStorage.setItem('filters', JSON.stringify(this.modifiedFilters));
+            // Sending filters to the parent component
+            this.$emit("filters", this.modifiedFilters);
         },
+        initializeFilters: function () {
+            this.modifiedFilters = {
+                ageValues: [18, 60],
+            };
+            localStorage.removeItem('filters');
+            this.$emit("filters", this.modifiedFilters);
+        },
+        closeModal: function () {
+            this.$modal.hide("filters");
+        },
+    },
+    mounted: function () {
+        this.readJobsForFilters();
+        this.readUsers();
+        this.readGroupsForFilters();
+        //Getting filters from localStorage, if they do exist - On va chercher les filtrs dans le local storage si ils ont été utlisés auparavant.
+        if (localStorage.getItem('filters')) {
+            this.modifiedFilters = JSON.parse(localStorage.getItem('filters'));
+        }
+
+    },
 };
 </script>
+
 <style scoped>
 .multiselect--active {
-        z-index: 10000;
+    z-index: 10000;
 }
+
 .slider {
-        width: 400px;
+    width: 400px;
 }
+
 .app-content {
-        padding: 40px 15px;
+    padding: 40px 15px;
 }
 </style>
